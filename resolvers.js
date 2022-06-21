c3da16506b35aa0a8a2ecf5020042ea7f097b9d3
MarisolCanavy <marisol.canavy@etud.univ-jfc.fr>
2022-06-21 17:33:42 +0200

    Ajout des unlocks et changement unlock state

--- resolvers.js
+++ resolvers.js
@@ -12,139 +12,119 @@ function saveWorld(context) {
   );
 }
 
-//porblème ici car la conversion 32 vers 64 bits ne se fait pas bien
 function majScore(context) {
   if(context.world){
     let tempsEcoule = Date.now() - parseInt(context.world.lastupdate);
 
     context.world.products.map((product) => {
       // option 1 : le produit n'a pas de manager débloquer
-      if(product.managerUnlocked == false){
+      if(product.managerUnlocked === false){
         //regarde si le produit est en production
         if(product.timeleft !== 0){
           //si le produit a été produit, on rajoute au monde son gain
-          if(product.timeleft > tempsEcoule){
+          if(product.timeleft < tempsEcoule){
             //plus tard il faudra rajouter le bonus des anges
             context.world.score += product.revenu;
+            context.world.money += product.revenu;
+            product.timeleft = 0;
           }else {
-            {console.log(tempsEcoule)}
             product.timeleft = product.timeleft - tempsEcoule;
           }
         }
       // option 2 : le produit a le manager débloquer
-      } else if (product.managerUnlocked == true){
+      } else if (product.managerUnlocked === true){
+        {console.log("MajScore : " + product.name + " a son manager débloqué")}
         context.world.score += Math.floor(tempsEcoule / product.vitesse)*product.revenu;
+        context.world.money += Math.floor(tempsEcoule / product.vitesse)*product.revenu;
         product.timeleft = parseInt(product.vitesse - (Math.floor(parseInt(tempsEcoule) / product.vitesse))*product.vitesse);
       }
-  });
-    
-  // mise à jour du lastupdate
-  context.world.lastupdate = Date.now().toString()
+    });
+    // mise à jour du lastupdate
+    context.world.lastupdate = Date.now().toString()
   }
 }
 
 module.exports = {
   Query: {
     getWorld(parent, args, context) {
-      {console.log(context.world.money)}
-      {console.log(context.user)}
-      saveWorld(context);
       majScore(context);
+      saveWorld(context);
       return context.world;
     }
   },
   Mutation: {
-    acheterQtProduit(parent, args, {world}) {
-      let productFind = null;
-
-      const products = world.products.map((product) => {
-        if (product.id == args.id){
-          let somme = product.cout*((1-Math.pow(product.croissance, product.quantite+args.quantite))/(1-product.croissance))-product.cout*((1-Math.pow(product.croissance, product.quantite))/(1-product.croissance))
+    acheterQtProduit(parent, args, context) {
+      majScore(context);
+      let productFind = context.world.products.find(p =>  p.id === args.id)
+      if(productFind){
+        let somme = productFind.cout*((1-Math.pow(productFind.croissance, productFind.quantite+args.quantite))/(1-productFind.croissance))-productFind.cout*((1-Math.pow(productFind.croissance, productFind.quantite))/(1-productFind.croissance))
           
-          if(world.money - somme >= 0){
-            if(parseInt(world.lastupdate)-product.timeleft>0){
-              majScore(world);
-            }
+        if(context.world.money - somme >= 0){
+          if(parseInt(context.world.lastupdate)-productFind.timeleft>0){
+            majScore(context);
+          }
+          context.world.money -= somme;
+          productFind.quantite += args.quantite;
+          context.world.lastupdate = Date.now().toString()
 
-            world.money -= somme;
-            product.quantite += args.quantite;
-            productFind = product;
-            world.lastupdate = Date.now().toString()
-          }else{
+          //regarder si on a atteint le pallier
+          let nextPalier = productFind.paliers.find( p => p.unlocked === false)
+          if(nextPalier){
+            if(productFind.quantite >= nextPalier?.seuil){
+              nextPalier.unlocked = true
+            }
+          }   
+      }else{
             console.error(err);
             throw new Error( `Le monde n'a pas assez d'argent ${context.money} pour acheter le produit ${args.quantite} x ${args.cout}.`)
           }
-        }
-        return productFind;
-      })
-
-      if (productFind == null) {
+      }else{
         console.error(err);
         throw new Error( `Le produit avec l'id ${args.id} n'existe pas`)
       }
-
-      saveWorld(world);
+      saveWorld(context);
+      majScore(context);
       return productFind;    
     },
     lancerProductionProduit(parent, args, context){
-      let productFind = null;
+      majScore(context);
 
-      console.log("read args of lancerProductionProduit " + args.id);
-      console.log("read world of lancerProductionProduit " + context.world.products);
-
-      context.world.products.map((product) => {
-        if (product.id == args.id){
-            product.timeleft = product.vitesse;
-            productFind = product;
-            majScore(context.world);
-
-            context.world.lastupdate = Date.now().toString()          }
-        return productFind;
-      })
-
-      if (productFind == null) {
+      let productFind = context.world.products.find(p =>  p.id === args.id)
+      if(productFind){
+        productFind.timeleft = productFind.vitesse
+        context.world.lastupdate = Date.now().toString()  
+      }else{
         console.error(err);
         throw new Error( `Le produit avec l'id ${args.id} n'existe pas`)
       }
-      console.log("found of lancerProductionProduit " + productFind.name);
-
-      saveWorld(context.world);
+      console.error(productFind.name + " a été lancé produit " + productFind.revenu);
+      saveWorld(context);
+      majScore(context);
       return productFind;  
     },
-    engagerManager(parent, {name}, {world}){
-      let managerFind = null;
-      let idManagee = null;
-      let produitManage = null;
-
-      const managers = world.managers.map((manager) => {
-        if (managers.name == name){
-          idManagee = manager.idcible;
-          managerFind = manager;
-          manager.unlocked = true;
-
-          const produits = world.products.map((product) => {
-            if (product.id == idManagee){
-              produitManage = product;
-              product.managerUnlocked = true;
-            }
-            return product;
-          })
-        }
-        return managerFind;
-      })
-
-      if (managerFind == null) {
+    engagerManager(parent, args, context){
+      majScore(context)      
+      let managerFind = context.world.managers.find(m =>  m.name === args.name)
+    
+      if(managerFind){
+        {console.log(" managerFind found : " + managerFind.name + " manager " + managerFind.unlocked)}
+        managerFind.unlocked = true
+        context.world.products.map((product)=> {console.log(product.id)})
+        let produitManage = context.world.products.find(p =>  p.id === managerFind.idcible )
+        if(produitManage){
+          produitManage.managerUnlocked = true
+        }else{
+          console.error(err);
+          throw new Error( `Le produit dont le manager est ${args.name} n'existe pas.`)
+        }  
+      }else{
         console.error(err);
         throw new Error( `Le manager avec le nom ${args.name} n'existe pas.`)
       }
 
-      if (produitManage == null) {
-        console.error(err);
-        throw new Error( `Le produit dont le manager est ${args.name} n'existe pas.`)
-      }
-
-      saveWorld(world);
-      return productFind; 
+      saveWorld(context);
+      majScore(context);
+      return managerFind; 
     }
   },
 };
